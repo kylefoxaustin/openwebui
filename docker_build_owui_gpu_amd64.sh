28ffb0c8d87c803e45c6405d09dea35735e62d07
Joseph K Fox <kylefoxaustin@gmail.com>
2025-03-27 11:49:55 -0500

    Update docker_build_owui_gpu_amd64.sh
    adding docker run command to run the built container

--- docker_build_owui_gpu_amd64.sh
+++ docker_build_owui_gpu_amd64.sh
@@ -1,1 +1,2 @@
-docker build -t owui_gpu_amd64 -f Dockerfile.GPU-amd64
+docker build -t owui_gpu_amd64 -f Dockerfile.GPU-amd64 .
+docker run -d -p 8080:8080 -p 11434:11434 -v ollama_data:/root/.ollama -v owui_data:/app/backend/data --name owui_gpu_amd64 owui_gpu_amd64
