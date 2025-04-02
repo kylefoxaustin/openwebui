f03df8faca40992b7a77fc7c5c92d60aa32459e5
Joseph K Fox <kylefoxaustin@gmail.com>
2025-04-02 12:24:11 -0500

    Delete docker_build_owui_cpu_only_amd64.sh

--- docker_build_owui_cpu_only_amd64.sh
+++
@@ -1,2 +0,0 @@
-docker build -t owui_cpu_only_amd64 -f Dockerfile.cpu-only
-docker run -d -p 8080:8080 -p 11434:11434 -v ollama_data:/root/.ollama -v owui_data:/app/backend/data --name owui_cpu_only owui_cpu_only_amd64
