02bfb8cde4c80c2d25023d28f187e69361857480
Joseph K Fox <kylefoxaustin@gmail.com>
2025-04-02 12:24:02 -0500

    Delete docker_build_owui_gpu_amd64.sh

--- docker_build_owui_gpu_amd64.sh
+++
@@ -1,2 +0,0 @@
-docker build -t owui_gpu_amd64 -f Dockerfile.GPU-amd64 .
-docker run -d -p 8080:8080 -p 11434:11434 -v ollama_data:/root/.ollama -v owui_data:/app/backend/data --name owui_gpu_amd64 owui_gpu_amd64
